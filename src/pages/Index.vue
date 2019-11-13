0cd96eb73d64e7f3e1d9daf9f17db831e837a142
Beatriz813 <beatrizalcantara813@gmail.com>
2019-11-13 12:51:38 -0300

    Primeiro Commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-page class="flex flex-center">
+    <div>
+      <p>Geolocation: {{ teste }}</p>
+      <q-btn label="geolocation" @click="getLocation()"/>
+    </div>
+  </q-page>
+</template>
+
+<script>
+// const onDeviceReady = () => {
+//   console.log('navigator.geolocation works well')
+// }
+// document.addEventListener('deviceready', onDeviceReady, false)
+export default {
+  name: 'PageIndex',
+  data () {
+    return {
+      posicionamento: {
+        coords: {
+          latitude: null
+        }
+      },
+      teste: null
+    }
+  },
+  methods: {
+    getLocation () {
+      navigator.geolocation.getCurrentPosition((posicionamento) => {
+        // this.posicionamento = posicionamento
+        this.teste = 'Olá'
+      })
+    }
+  }
+}
+</script>
